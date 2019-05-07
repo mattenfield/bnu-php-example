bf2698a149a3532f91a29a3e425daf33448f0c59
Matthew Enfield <21700581@bucks.ac.uk>
2019-05-07 19:50:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
-   echo "<h2> Welcome to the College Platform </h2>";
    include("_includes/config.inc");
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
-
    echo template("templates/partials/header.php");
-
    if (isset($_GET['return'])) {
       $msg = "";
       if ($_GET['return'] == "fail") {$msg = "Login Failed. Please try again.";}
       $data['message'] = "<p>$msg</p>";
    }
-
    if (isset($_SESSION['id'])) {
       $data['content'] = "<p>Welcome to your dashboard.";
       echo template("templates/partials/nav.php");
@@ -19,9 +15,6 @@
    } else {
       echo template("templates/login.php", $data);
    }
-
    echo template("templates/partials/footer.php");
-
    // another test edit
-
 ?>
