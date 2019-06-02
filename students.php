f061b6ee78f940b738dbc1a972d6e90d71b630b8
Matthew Enfield <21700581@bucks.ac.uk>
2019-06-02 14:09:15 +0100

    Create students.php
    completed part 2 of Assignment - created students.php - cloned modules.php and changed parameters to match student table.

---
+++ students.php
@@ -0,0 +1,41 @@
+<?php
+
+   include("_includes/config.inc");
+   include("_includes/dbconnect.inc");
+   include("_includes/functions.inc");
+
+
+   // check logged in
+   if (isset($_SESSION['id'])) {
+
+      echo template("templates/partials/header.php");
+      echo template("templates/partials/nav.php");
+
+      // Build SQL statment that selects a student's modules
+      $sql = "SELECT studentid,dob,firstname,lastname,house,town,county,country,postcode FROM student;";
+
+      $result = mysqli_query($conn,$sql);
+
+      // prepare page content
+      $data['content'] .= "<table border='1'>";
+      $data['content'] .= "<tr><th colspan='9' align='center'>Students</th></tr>";
+      $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th></tr>";
+      // Display the modules within the html table
+      while($row = mysqli_fetch_array($result)) {
+         $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[dob] </td>";
+         $data['content'] .= "<td> $row[firstname] </td><td> $row[lastname] </td>
+         <td> $row[house] </td><td> $row[town] </td><td> $row[county] </td><td> $row[country] </td>
+         <td> $row[postcode] </td></tr>";
+      }
+      $data['content'] .= "</table>";
+
+      // render the template
+      echo template("templates/default.php", $data);
+
+   } else {
+      header("Location: index.php");
+   }
+
+   echo template("templates/partials/footer.php");
+
+?>
