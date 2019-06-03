8dacc152c68aad2d5483a3d0f34a455317daa0b8
Matthew Enfield <21700581@bucks.ac.uk>
2019-06-03 14:53:52 +0100

    Update students.php
    Complete PHP Task 3 - delete functionality has now been added.

--- students.php
+++ students.php
@@ -4,7 +4,6 @@
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
 
-
    // check logged in
    if (isset($_SESSION['id'])) {
 
@@ -16,18 +15,34 @@
 
       $result = mysqli_query($conn,$sql);
 
-      // prepare page content
+      $data['content'] .= '<form method="post">';
       $data['content'] .= "<table border='1'>";
-      $data['content'] .= "<tr><th colspan='9' align='center'>Students</th></tr>";
-      $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th></tr>";
+      $data['content'] .= "<tr><th colspan='10' align='center'>Students</th></tr>";
+      $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th>
+      <th>County</th><th>Country</th><th>Postcode</th><th>Select</th></tr>";
       // Display the modules within the html table
       while($row = mysqli_fetch_array($result)) {
          $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[dob] </td>";
          $data['content'] .= "<td> $row[firstname] </td><td> $row[lastname] </td>
          <td> $row[house] </td><td> $row[town] </td><td> $row[county] </td><td> $row[country] </td>
-         <td> $row[postcode] </td></tr>";
+         <td> $row[postcode] </td> </td> ";
+          $data['content'] .= "<td> <input type ='checkbox' name='delrecords[]' value='".$row['studentid']."' </td></tr> </td>";
       }
       $data['content'] .= "</table>";
+      $data['content'] .= "</br></br></br>";
+      $data['content'] .= '<input type="submit" name="delete" value="Delete Records">';
+      $data['content'] .= "</form>";
+
+      if(isset($_POST['delete'])){
+        $checkboxcount = count($_POST['delrecords']);
+        $i=0;
+        while($i<$checkboxcount){                                 //count of records to delete
+          $theid = $_POST['delrecords'][$i];
+          mysqli_query($conn, "DELETE FROM student WHERE studentid= '$theid'");
+          $i++;
+        }
+        header("Location: students.php");
+      }
 
       // render the template
       echo template("templates/default.php", $data);
@@ -36,6 +51,6 @@
       header("Location: index.php");
    }
 
-   echo template("templates/partials/footer.php");
-
-?>
+   echo template("templates/partials/footer.php"); 
+   mysqli_close($conn);
+   ?>
